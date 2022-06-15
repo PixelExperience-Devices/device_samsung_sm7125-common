c4a0203daf175330e7f276f3eded711e7506b450
Rashed Abdel-Tawab <rashed@linux.com>
2022-06-15 14:45:20 +0200

    sm7125: Import audio@7.0 default impl
    Change-Id: I37dac8d63dff454a396e9c14a1062db7f4b2588d

--- common.mk
+++ common.mk
@@ -67,8 +67,8 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
-    android.hardware.audio@7.0-impl \
-    android.hardware.audio.effect@7.0-impl \
+    android.hardware.audio@6.0-impl.sm7125 \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
     audio.r_submix.default \
