71e04fc98e0edc44a2203e5163b0cc2f91c04819
menorziin <williamsilvapppl@gmail.com>
2022-10-08 02:13:09 +0300

    sm7125-common: Build android.frameworks.sensorservice@1.0 HAL
    * E init : Control message: Could not find 'android.frameworks.sensorservice@1.0::ISensorManager/default' for ctl.interface_start from pid: 516 (/system/bin/hwservicemanager)
    
    Change-Id: I5a4feba0812bcbb187ef8a1c2298f799509e6c92

--- common.mk
+++ common.mk
@@ -133,6 +133,8 @@ PRODUCT_PACKAGES += \
 
 # GNSS
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0 \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.gnss@2.1.vendor
 
 # Graphics
