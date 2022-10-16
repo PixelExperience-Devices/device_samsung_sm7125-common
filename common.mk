dc1c0b8f42e27ef160650dfd2538555380addf1b
Simon1511 <simon2002.schoenmackers@gmail.com>
2022-10-16 17:24:34 +0200

    sm7125-common: Satisfy EPPE enforcement
    build/make/core/main.mk:1308: warning:  device/samsung/a52q/lineage_a52q.mk includes non-existent modules in PRODUCT_PACKAGES
    Offending entries:
    android.hardware.omx@1.0-service
    com.qualcomm.qti.bluetooth_audio@1.0.vendor
    libaudioalsa
    build/make/core/main.mk:1308: error: Build failed.
    
    Change-Id: I94d22518273f983d2bb3b211334d0a95d09a4a0e

--- common.mk
+++ common.mk
@@ -77,7 +77,6 @@ PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.2-impl \
     audio.r_submix.default \
     audio.usb.default \
-    libaudioalsa \
     libtinycompress \
     libqcomvisualizer \
     libqcomvoiceprocessing \
@@ -110,7 +109,6 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    com.qualcomm.qti.bluetooth_audio@1.0.vendor \
     audio.bluetooth.default \
     android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.bluetooth@1.0.vendor
@@ -271,7 +269,6 @@ PRODUCT_COPY_FILES += \
 
 # OMX
 PRODUCT_PACKAGES += \
-    android.hardware.omx@1.0-service \
     libmm-omxcore \
     libOmxAacEnc \
     libOmxAmrEnc \
