7483543edf516b6ac8f7828bb14386776e2273bf
Ruchit Marathe <ruchitmarathe@gmail.com>
2022-12-26 14:37:09 +0530

    sm7125: Properly build memtrack

--- common.mk
+++ common.mk
@@ -152,7 +152,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     vendor.qti.hardware.display.allocator-service \
-    android.hardware.memtrack-service.sm7125 \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl \
     libtinyxml \
     libtinyxml2 \
